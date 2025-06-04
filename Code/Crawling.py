77b159d1b8b5cc678eabd3ca62a98778a34cde18
Dely Rafael Sefriansya <110448941+delyrafael@users.noreply.github.com>
2025-06-04 11:14:38 +0700

    Update Crawling.py

--- Code/Crawling.py
+++ Code/Crawling.py
@@ -43,6 +43,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 import random
 import logging
